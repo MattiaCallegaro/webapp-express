d126a43326aaaf0c876952aaced4dde5c50d5ace
MattiaCallegaro <mattiacallegarocioi@gmail.com>
2025-06-03 18:21:30 +0200

    [FIX] Fix img path

--- middlewares/imagePath.js
+++ middlewares/imagePath.js
@@ -1,7 +1,8 @@
 const setimagePath = (req, res, next) => {
-    console.log(`${req.protocol}://${req.get('host')}/img/films`)
-    req.imagePath = `${req.protocol}://${req.get('host')}/img/films/`
+    req.imagePath = `${req.protocol}://${req.get('host')}/imgs/films/`
     next()
 }
 
+
 module.exports = setimagePath
+
